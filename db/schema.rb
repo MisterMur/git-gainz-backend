b31ae3dc0f2c261e84afa0e3dcfc57280f5f0989
MisterMur <brian.j.murillo@gmail.com>
2019-12-19 08:48:45 -0500

    add phone column to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_13_234707) do
+ActiveRecord::Schema.define(version: 2019_12_19_125308) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -65,6 +65,7 @@ ActiveRecord::Schema.define(version: 2019_12_13_234707) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string "access_token"
+    t.string "phone"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
