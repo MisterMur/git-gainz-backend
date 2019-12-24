ad7e191ec640be767556ba59092732639d383a99
MisterMur <brian.j.murillo@gmail.com>
2019-12-24 15:37:07 -0500

    change devise user migration to default createtime

--- db/migrate/20191213230954_add_devise_to_users.rb
+++ db/migrate/20191213230954_add_devise_to_users.rb
@@ -12,7 +12,7 @@ class AddDeviseToUsers < ActiveRecord::Migration[5.2]
       t.datetime :reset_password_sent_at
 
       ## Rememberable
-      t.datetime :remember_created_at
+      t.datetime :remember_created_at, null: false, default:''
 
       ## Trackable
       # t.integer  :sign_in_count, default: 0, null: false
